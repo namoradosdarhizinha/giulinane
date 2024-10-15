77101a0924a9416d350c484250ca0609e0de0222
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-15 11:23:09 -0300

    ewf

---
+++ main.js
@@ -0,0 +1,16 @@
+function criacartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document. creatElement ('article');
+    cartao.classname = 'cartao';
+cartao.innerHTML = ` 
+</html>
+<div class = 'conteudo-cartao'
+<h3> categoria </h3>
+<div class="pergunta-cartao">
+<p>pergunta</p>   </div>
+<div class= " resposta-cartao"
+<p> resposta
+<p/>   </div>
+</div>
+`
+}
