7f17c449d959b8238e6d6a5db6381c59fa235940
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-15 11:50:06 -0300

    asf

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criacartao(
+    'quimica',
+    'o que é um átomo?',
+    'a unidade básica de construção da materia'
+)
+criacartao(
+    'historia',
+    'quando getuluio vargas morreu?',
+    '1954'
+)
+criacartao(
+    'quimica',
+    'o que vem antes do átomo?',
+    'quark, gluon, eletron e fóton'
+)
+criacartao(
+    'historia',
+    'quando começou a ditadura militar no brasil?',
+    '1964'
+)
+criacartao(
+    'geografia',
+    'onde fica a cordilheirea dos andes?',
+    'america do sul'
+)
+criacartao(
+    'historia',
+    'quem fundou o CEP?',
+    'marechal manoel de fonseca lima e silva'
+)
+criacartao(
+    'biologia',
+    'o que é um cromossomo?',
+    'estruturas que abrigam o material genetico dentro da célula'
+)
+criacartao(
+    'biologia',
+    'quais são os tres tipos de cromossomos?',
+    'metacentrico, acrocentrico e submetacentrico'
+)
+criacartao(
+    'biologia',
+    'o que é um alelo?',
+    'diferentes versoes de um gene que ocupam uma mesma posição no cromossomo'
+)
+criacartao(
+    'história',
+    'como vargas chegou ao poder?',
+    'atraves de um golpe de estado'
+)
+criacartao(
+    'história',
+    'qual foi o motivo da 2 guerra mundial?',
+    'o expansionismo territorial germanico'
+)
+criacartao(
+    'matematica',
+    'o que é uma matriz?',
+    'é uma maneira de representar dados dividindo-os em linhas e colunas'
+)
+criacartao(
+    'biologia',
+    'o que são vertebrados?',
+    'animais que apresentam coluna vertebral e cranio'
+)
+criacartao(
+    'biologia',
+    'o que são invertebrados?',
+    'são animais que não apresentam osso'
+)
+criacartao(
+      'história',
+      'como vargas morreu?',
+      'suicidio'
+)
+criacartao(
+    'geografia',
+    'onde fica o saara?',
+    'no norte da frica'
+)
+criacartao(
+    'história',
+    'quantos anos tem o presidente do brasil?',
+    '78'
+)
+criacartao(
+    'historia',
+    'quem é o presidente do eua?',
+    'joe biden'
+)
+criacartao(
+    'geografia',
+    'onde fica o havai?',
+    'EUA'
+)
+criacartao(
+    'história',
+    'quem é o presidente da coreia do norte?',
+    'kim jong-un'
+)
