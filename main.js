6bdf0803880a1729430bfc8670de9ef5da6fd756
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-15 12:05:11 -0300

    sdfv

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ function criacartao (categoria, pergunta, resposta){
 cartao.innerHTML = ` 
 </html>
 <div class = 'conteudo-cartao'
-<h3> categoria </h3>
+<h3> ${categoria} </h3>
 <div class="pergunta-cartao">
-<p>pergunta</p>   </div>
+<p>${pergunta}</p>   </div>
 <div class= " resposta-cartao"
-<p> resposta
-<p/>   </div>
+<p> ${resposta} <p/>   </div>
 </div>
 `
 container.appendChild(cartao)
