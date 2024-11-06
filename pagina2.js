46a9aa61e56b1e5bf3c2d64ae7839dfa9b0213d5
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-11-06 11:43:21 -0300

    rgre

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criacartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = ` 
-<div class = "conteudo-cartao">
+<div class = "conteudo-cartao" onclick="mostraresposta(this) ">
 <h3> ${categoria} </h3>
 <div class="pergunta-cartao">
 <p>${pergunta}</p>   
@@ -17,4 +17,14 @@ container.appendChild(cartao)
 }
 function mostraresposta(card){
     const resposta =card.carySelection('.resposta-cartao');
+    if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+
+    }
+ else{
+resposta.style.display= 'none';
+
+ }
+
+
 }
